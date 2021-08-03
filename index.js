896d05c8be18f3aeb0b540d5a589110088942e30
Farhan Salam <67406571+mfarhansalam@users.noreply.github.com>
2021-08-04 01:35:28 +0800

    Update index.js
    customer

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const mysql = require('mysql');
 
 var con = mysql.createPool({
     host: "us-cdbr-east-04.cleardb.com",
-    user: "b28d060c44e16b",
-    password: "41a87cf8",
-    database: "heroku_5040d734006a0f4"
+    user: "b9d191c32faac0",
+    password: "f4a019ef",
+    database: "heroku_76c19b622dd9a4e"
 });
 
 app.get('/', (req, res) => {
     con.getConnection(function(err, tempconnection) {
         if (err) { res.send("Error occured!"); } else {
-            var sql = "SELECT * FROM netfreak";
+            var sql = "SELECT * FROM customer";
             con.query(sql, function(err, result, fields) {
                 if (err) { throw err; } else {
                     res.send(result);
